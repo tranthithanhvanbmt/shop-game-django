b4631f1333a5e314a9a9e15607b8e782b843d97b
Thanh Van <tranthithanhvanbmt@gmail.com>
2026-04-06 06:01:17 +0700

    Update AppConfig names to match full module paths

--- shop_game/billing/apps.py
+++ shop_game/billing/apps.py
@@ -3,7 +3,7 @@ from django.apps import AppConfig
 
 class BillingConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'billing'
+    name = 'shop_game.billing'
     verbose_name = '3. THẺ CÀO & TOPUP'
 
     def ready(self):
