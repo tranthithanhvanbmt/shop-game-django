dc01f618f4dcdd4d170a2c6c47b2829047cd18c0
Thanh Van <tranthithanhvanbmt@gmail.com>
2026-04-06 09:32:00 +0700

    Update static settings and add static directory

--- shop_game/settings.py
+++ shop_game/settings.py
@@ -143,6 +143,12 @@ USE_TZ = False
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+]
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
